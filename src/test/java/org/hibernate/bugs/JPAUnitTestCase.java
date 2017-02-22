7ff056a9cbc3d746e655f47eecb8dcc6848c19ca
Giovanni Lovato <giovanni.lovato@saiv.it>
2017-02-22 18:04:36 +0100

    chore(*): clean up PU names and comments

--- src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
+++ src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
@@ -6,21 +6,18 @@ import javax.persistence.Persistence;
 import org.junit.After;
 import org.junit.Before;
 
-/**
- * This template demonstrates how to develop a test case for Hibernate ORM, using the Java Persistence API.
- */
 public class JPAUnitTestCase {
 
-	private EntityManagerFactory entityManagerFactory;
+    private EntityManagerFactory entityManagerFactory;
 
-	@Before
-	public void init() {
-		this.entityManagerFactory = Persistence.createEntityManagerFactory( "templatePU" );
-	}
+    @Before
+    public void init() {
+        this.entityManagerFactory = Persistence.createEntityManagerFactory("h2");
+    }
 
-	@After
-	public void destroy() {
-		this.entityManagerFactory.close();
-	}
+    @After
+    public void destroy() {
+        this.entityManagerFactory.close();
+    }
 
 }
